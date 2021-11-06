37457d830f32a6ffc2e7ec5b5e8034cb5d726ec1
ArtiomSu <artiom.sumigora@mycit.ie>
2021-11-06 01:07:08 +0000

    merged macos layout and other oses layout into one. Can switch between the different macos and pc layers on the fly seamelessly

--- keyboards/ergodox_ez/keymaps/artiom_no_mouse_joined_with_mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/artiom_no_mouse_joined_with_mac/keymap.c
@@ -1,14 +1,27 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 
-#include <stdbool.h> //for boolean
+#define bool _Bool
+#define true 1
+#define false 0
 
 // layer defines
+/*
+    Layers accessing other layers must be under the layer that they access.
+    For example, if layer 1 is accessing layer 2, layer 2 must be after layer 1.
+*/
 #define Layer_main 0
-#define Layer_symbols 1
-#define Layer_macros 2
-#define Layer_gaming 3
+#define Layer_mac_main 1
+#define Layer_symbols 2
+#define Layer_mac_symbols 3
+#define Layer_macros 4
+#define Layer_gaming 5
 
+// layer Colours since they are different from layers themselves now
+#define Layer_main_colour 0
+#define Layer_symbols_colour 1
+#define Layer_macros_colour 2
+#define Layer_gaming_colour 3
 
 
 enum custom_keycodes {
@@ -64,6 +77,7 @@ int hue_amount = 0;
 bool main_layer_brightness = true; // can disable the main layer rgb individually
 
 bool combos_on = true; // use combo feature by default
+bool mac_mode = false;
 
 int word_length_count = 0;
 int last_word_length=0;
@@ -101,17 +115,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,             KC_Q,           KC_W,               KC_E,           KC_R,           KC_T,           LCTL(LSFT(KC_LGUI)),                            KC_F2,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           ST_MACRO_SSH,
     LCTL(KC_LALT),      KC_A,           KC_S,               KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_RALT,
     KC_LSHIFT,          KC_Z,           KC_X,               KC_C,           KC_V,           KC_B,           KC_HYPR,                                        KC_F4,          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RCTRL,
+    KC_LCTRL,           KC_LALT,        LCTL(LSFT(KC_LGUI)),LCTL(KC_LGUI),  LSFT(KC_LGUI),                                                                                                  KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_LEAD,
+
+
+
+                                                                                                    LSFT(KC_PSCREEN),KC_PSCREEN,                            DYN_MACRO_PLAY1,DYN_MACRO_PLAY2,
+                                                                                                                        KC_HOME,                            ST_MACRO_VIM_wq,
+                                                        LT(Layer_symbols,KC_BSPACE),LGUI_T(KC_DELETE),           LCTL(KC_SPACE),                            LSFT_T(KC_ESCAPE),              LT(Layer_macros,KC_ENTER), KC_SPACE
+  ),
+
+  [Layer_mac_main] = LAYOUT_ergodox_pretty(
+    KC_GRAVE,           KC_1,           KC_2,               KC_3,           KC_4,           KC_5,           KC_MINUS,                                       KC_PLUS,        KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           LSFT(KC_ENTER),
+    KC_TAB,             KC_Q,           KC_W,               KC_E,           KC_R,           KC_T,           LCTL(LSFT(KC_LGUI)),                            KC_F2,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           ST_MACRO_SSH,
+    LCTL(KC_LALT),      KC_A,           KC_S,               KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_RALT,
+    KC_LSHIFT,          KC_Z,           KC_X,               KC_C,           KC_V,           KC_B,           KC_HYPR,                                        KC_F4,          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RCTRL,
     KC_LGUI,            KC_LALT,        LCTL(LSFT(KC_LGUI)),LCTL(KC_LGUI),  LSFT(KC_LGUI),                                                                                                  KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_LEAD,
 
 
 
                                                                                                     LSFT(KC_PSCREEN),LCTL(LGUI(LSFT(KC_4))),                DYN_MACRO_PLAY1,DYN_MACRO_PLAY2,
                                                                                                                         KC_HOME,                            ST_MACRO_VIM_wq,
-                                                                    LT(1,KC_BSPACE),LCTL_T(KC_DELETE),           LCTL(KC_SPACE),                            LSFT_T(KC_ESCAPE),              LT(2,KC_ENTER), KC_SPACE
+                                                    LT(Layer_mac_symbols,KC_BSPACE),LCTL_T(KC_DELETE),           LCTL(KC_SPACE),                            LSFT_T(KC_ESCAPE),              LT(Layer_macros,KC_ENTER), KC_SPACE
   ),
 
 
+
+
   [Layer_symbols] = LAYOUT_ergodox_pretty(
+    LSFT(KC_GRAVE),     KC_F1,          KC_F2,              KC_F3,                  KC_F4,              KC_F5,              KC_F11,                          KC_F12,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F13,
+    _______,            KC_GRAVE,       KC_QUOTE,           LSFT(KC_QUOTE),         LSFT(LCTL(KC_C)),   LSFT(LCTL(KC_V)),   KC_CIRC,                        _______,        KC_LABK,        KC_PLUS,        KC_MINUS,       KC_RABK,        KC_EQUAL,       _______,
+    _______,            KC_EXLM,        LSFT(KC_2),         LSFT(KC_3),             KC_DLR,             KC_PERC,                                                            KC_ASTR,        KC_LBRACKET,    KC_RBRACKET,    KC_UNDS,        KC_QUES,        _______,
+    KC_LSHIFT,          ST_M_n_equal,   KC_AMPR,            LSFT(KC_NONUS_BSLASH),  LSFT(KC_NONUS_HASH),KC_ASTR,            KC_CIRC,                        _______,        KC_AMPR,        KC_LCBR,        KC_RCBR,        KC_DOT,         KC_NONUS_BSLASH,KC_LSHIFT,
+    LALT(KC_LCTRL),     ST_M_n_equal_2, LCTL(KC_X),         LCTL(KC_C),             LCTL(KC_V),                                                                                             LSFT(KC_9),     LSFT(KC_0),     ST_M_l_equal,   ST_M_g_equal,   KC_LALT,
+
+
+                                                                                                                    _______,_______,                        _______, _______,
+                                                                                                                            _______,                        _______,
+                                                                                        _______, _______,                   _______,                        LSFT(KC_SCOLON),                KC_NONUS_HASH,  KC_SCOLON
+  ),
+
+  [Layer_mac_symbols] = LAYOUT_ergodox_pretty(
     LSFT(KC_GRAVE),     KC_F1,          KC_F2,              KC_F3,                  KC_F4,              KC_F5,              KC_F11,                          KC_F12,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F13,
     _______,            KC_GRAVE,       KC_QUOTE,           LSFT(KC_2),             LGUI(LCTL(KC_C)),   LGUI(LCTL(KC_V)),   KC_CIRC,                        _______,        KC_LABK,        KC_PLUS,        KC_MINUS,       KC_RABK,        KC_EQUAL,       _______,
     _______,            KC_EXLM,        LSFT(KC_QUOTE),     LSFT(KC_3),             KC_DLR,             KC_PERC,                                                            KC_ASTR,        KC_LBRACKET,    KC_RBRACKET,    KC_UNDS,        KC_QUES,        _______,
@@ -123,6 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                                             _______,                        _______,
                                                                                         _______, _______,                   _______,                        LSFT(KC_SCOLON),                LALT(KC_3),     KC_SCOLON
   ),
+
 
 
   [Layer_macros] = LAYOUT_ergodox_pretty(
@@ -143,7 +187,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_0,               KC_1,           KC_2,               KC_3,           KC_4,           KC_5,           KC_6,                                           KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         KC_HOME,
     KC_ESCAPE,          KC_8,           KC_Q,               KC_W,           KC_E,           KC_R,           KC_7,                                           KC_F6,          KC_KP_ASTERISK, KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_MINUS,    KC_END,
     KC_TAB,             KC_9,           KC_A,               KC_S,           KC_D,           KC_F,                                                                           KC_KP_SLASH,    KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_PLUS,     KC_DELETE,
-    KC_LSHIFT,          KC_LSHIFT,      KC_X,               KC_C,           KC_V,           KC_G,           KC_ENTER,                                       KC_F5,          KC_KP_ENTER,    KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_UP,          TO(0),
+    KC_LSHIFT,          KC_LSHIFT,      KC_X,               KC_C,           KC_V,           KC_G,           KC_ENTER,                                       KC_F5,          KC_KP_ENTER,    KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_UP,          TO(Layer_main),
     KC_LCTRL,           KC_Z,           KC_I,               KC_C,           KC_B,                                                                                                           KC_KP_0,        KC_KP_DOT,      KC_LEFT,        KC_DOWN,        KC_RIGHT,
                                                                                                                         KC_Y, KC_U,                         KC_F3, KC_F4,
                                                                                                                               KC_O,                         KC_F2,
@@ -201,7 +245,7 @@ void suspend_wakeup_init_user(void) {
 
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
     //             8                      9                         0                       -                       =
-    [Layer_main] =
+    [Layer_main_colour] =
             { LM_Light_orange,      KM_Light_red,           KM_dirty_green,         KM_dirty_green,         KM_dirty_green,    \
     //             y                      u                         i                       o                       p
             KM_Light_yellow,        LM_Light_orange,        KM_Light_red,           KM_dirty_green,         KM_dirty_green,     \
@@ -227,7 +271,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
 
 
 
-    [Layer_symbols] =
+    [Layer_symbols_colour] =
             { LM_Slight_dark_red,   LM_Slight_dark_red,     LM_Slight_dark_red,     LM_Slight_dark_red,     LM_Slight_dark_red, \
 
             LM_Blue,                LM_LightOrange,         LM_LightOrange,         LM_Blue,                LM_LightOrange,     \
@@ -253,7 +297,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
 
 
     //             8                    9                           0                    -                       =
-    [Layer_macros] =
+    [Layer_macros_colour] =
             { LM_______,            LM_Blooder_Red,         LM_NeonGreen,           LM_______,              LM_______,          \
     //             y                    u                           i                    o                       p
             LM_______,              LM_Blooder_Red,         LM_NeonGreen,           LM_______,              LM_______,          \
@@ -278,7 +322,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
 
 
 
-    [Layer_gaming] =
+    [Layer_gaming_colour] =
             { LM_LightBrown,        LM_LightBrown,          LM_LightBrown,          LM_LightBrown,          LM_LightBrown,      \
 
             LM_Light_orange,        LM_White,               LM_White,               LM_White,               LM_White,           \
@@ -363,7 +407,6 @@ const uint16_t PROGMEM combo_delete_word[] = {KC_F, KC_DOWN, COMBO_END};
 const uint16_t PROGMEM combo_alt_f4[] = {KC_F, KC_4, COMBO_END};
 const uint16_t PROGMEM combo_scroll_up[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_scroll_down[] = {KC_E, KC_R, COMBO_END};
-
 
 combo_t key_combos[COMBO_COUNT] = {
     [COMBO_LEFT_SQUARE_BRACKET] = COMBO_ACTION(combo_left_square_bracket),
@@ -573,7 +616,14 @@ void set_layer_color(int layer) {
             use_default_lighting = false;
         }
 
-        if(combos_on && i == 47){
+        if(combos_on && i == 47){ // show indicator on key bellow z.
+            if(modifiers_blink_count < 100){
+                rgb_matrix_set_color( i, 237, 28, 28);
+                use_default_lighting = false;
+            }
+        }
+
+        if(mac_mode && i == 16){ // show indicator on key m
             if(modifiers_blink_count < 100){
                 rgb_matrix_set_color( i, 237, 28, 28);
                 use_default_lighting = false;
@@ -630,17 +680,24 @@ void rgb_matrix_indicators_user(void) {
     if(rgb_show && !rgb_timed_out){
         switch (biton32(layer_state)) {
             case Layer_main:
-                set_layer_color(Layer_main);
+                set_layer_color(Layer_main_colour);
             break;
             case Layer_symbols:
-                set_layer_color(Layer_symbols);
+                set_layer_color(Layer_symbols_colour);
             break;
             case Layer_macros:
-                set_layer_color(Layer_macros);
+                set_layer_color(Layer_macros_colour);
             break;
             case Layer_gaming:
-                set_layer_color(Layer_gaming);
+                set_layer_color(Layer_gaming_colour);
             break;
+            case Layer_mac_main:
+                set_layer_color(Layer_main_colour);
+            break;
+            case Layer_mac_symbols:
+                set_layer_color(Layer_symbols_colour);
+            break;
+
             default:
                 if (rgb_matrix_get_flags() == LED_FLAG_NONE){
                     rgb_matrix_set_color_all(0, 0, 0);
@@ -833,7 +890,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-uint32_t layer_state_set_user(uint32_t state) { return state; };
+uint32_t layer_state_set_user(uint32_t state) {
+    if(state == Layer_main){ // game layer always returns to Layer_main so need to update mac_mode accordingly
+        mac_mode = false;
+    }else if(state == Layer_mac_main){
+        mac_mode = true;
+    }
+    return state;
+};
 
 LEADER_EXTERNS();
 int did_leader_succeed;
@@ -855,10 +919,6 @@ void matrix_scan_user(void) {
       SEND_STRING("10.0.0.69:19009" SS_TAP(X_ENTER) SS_DELAY(700) "pirate" SS_TAP(X_TAB));
       did_leader_succeed = true;
     } else
-    SEQ_ONE_KEY(KC_H) {
-      SEND_STRING("hahahahahahahahahahaha" );
-      did_leader_succeed = true;
-    } else
     SEQ_ONE_KEY(KC_C) {
       SEND_STRING("code ." SS_TAP(X_ENTER));
       did_leader_succeed = true;
@@ -869,7 +929,7 @@ void matrix_scan_user(void) {
     } else
     SEQ_TWO_KEYS(KC_RIGHT, KC_RIGHT) { //toggle leds on/off
         rgb_show = !rgb_show;
-      did_leader_succeed = true;
+        did_leader_succeed = true;
     } else
     SEQ_THREE_KEYS(KC_RIGHT, KC_RIGHT, KC_RIGHT){ //turn on leds and wake and reset brightness
         rgb_timed_out = false;
@@ -883,17 +943,27 @@ void matrix_scan_user(void) {
       SEND_STRING(SS_LSFT("'") "t80search_bot ");
       did_leader_succeed = true;
     } else
-    SEQ_ONE_KEY(KC_UP) {
-      layer_on(3);
+    SEQ_TWO_KEYS(KC_G, KC_G) {
+      layer_move(Layer_gaming);
       did_leader_succeed = true;
     } else
     SEQ_TWO_KEYS(KC_C, KC_O) {
-      SEND_STRING("cd /media/veracrypt1/GIT/qmk_firmware && make clean && make ergodox_ez:artiom_no_mouse_mac");
+      SEND_STRING("cd /media/veracrypt1/GIT/qmk_firmware && make clean && make ergodox_ez:artiom_no_mouse_joined_with_mac");
       did_leader_succeed = true;
     } else
     SEQ_ONE_KEY(KC_LEFT){
         combo_toggle();
         combos_on = is_combo_enabled();
+        did_leader_succeed = true;
+    } else
+    SEQ_THREE_KEYS(KC_M, KC_M, KC_M){
+        mac_mode = !mac_mode;
+        if(mac_mode){
+            layer_move(Layer_mac_main);
+        }else{
+            layer_move(Layer_main);
+        }
+        did_leader_succeed = true;
     }
     leader_end();
   }
