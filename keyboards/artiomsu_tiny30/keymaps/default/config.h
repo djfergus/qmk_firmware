8959d1aafc89e7b2d5a66355d494178b96766d6a
ArtiomSu <artiom.sumigora@mycit.ie>
2021-11-23 21:54:38 +0000

    basic layout and basic readme

---
+++ keyboards/artiomsu_tiny30/keymaps/default/config.h
@@ -0,0 +1,23 @@
+#undef NO_DEBUG
+#define NO_DEBUG
+
+#undef NO_PRINT
+#define NO_PRINT
+
+// force nkro
+#define FORCE_NKRO
+
+// Leader key defines
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 500
+
+//combos
+#define COMBO_COUNT 24
+#define COMBO_TERM 30
+
+
+// auto shift
+#define AUTO_SHIFT_TIMEOUT 150
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_NUMERIC
+#define AUTO_SHIFT_REPEAT
