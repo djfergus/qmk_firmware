ee79babc08022bf43bd330042a02e289ff4c0d64
ArtiomSu <doasartiom@gmail.com>
2023-08-06 22:21:12 +0100

    led strip support

--- keyboards/artiomsu_dactyl/config.h
+++ keyboards/artiomsu_dactyl/config.h
@@ -19,12 +19,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // WS2812 RGB LED strip input and number of LEDs
-#define RGBLED_NUM 1
+#define RGBLED_NUM 36
+//#define RGBLED_SPLIT 36
 
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_DRIVER SIOD0
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
+#define SERIAL_PIO_USE_PIO0 // use state machine 0 for serial split
+#define WS2812_PIO_USE_PIO1 // use state machine 1 for rgb
 
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
@@ -34,3 +37,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MASTER_RIGHT
 
 //#define EE_HANDS
+
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_TWINKLE
+
+
+#define RGBLIGHT_EFFECT_SNAKE_LENGTH 5
